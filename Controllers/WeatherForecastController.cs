305afff880589a1a9f54ed997b1a1f8a8eee2413
Kjell Otto EXT <info@kjell-otto.de>
2020-08-11 12:53:49 +0200

    chore: added todo
    feat: add file

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace GitTalk.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            // TODO(@kjellski): added todo
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
